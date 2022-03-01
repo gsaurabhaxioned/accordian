bb0f95acf517538ea1185affbfe89bfd53c318bb
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-01 09:20:53 +0530

    js code added for accordian

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,12 @@ $(document).ready(function () {
     });
 });
 
+let accordian_label = document.querySelectorAll('.explore'),
+    accordian = document.querySelectorAll('.recent-project-cards');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+accordian_label.forEach((i, index) => {
+    i.addEventListener('click', function () {
+        accordian[index].classList.toggle('recent-project-cards-explore');
+        this.classList.toggle('hide');
+    })
+})
